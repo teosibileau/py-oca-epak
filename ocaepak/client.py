eab826177b31331e7a9f5726141efe420b34388e
Teofilo Sibileau <teo.sibileau@gmail.com>
2026-02-28 18:57:54 -0300

    Implement logger instance

--- ocaepak/client.py
+++ ocaepak/client.py
@@ -5,63 +5,55 @@ from datetime import datetime
 
 
 import os
+import logging
 from jinja2 import Template
 
 ROOT = os.path.dirname(os.path.realpath(__file__))
+logger = logging.getLogger(__name__)
 
 
 class OcaService(object):
-    WSDL_BASE_URI = 'http://webservice.oca.com.ar/oep_tracking/Oep_Track.asmx'
-    OCA_WDSL = '%s?WSDL' % WSDL_BASE_URI
+    WSDL_BASE_URI = "http://webservice.oca.com.ar/oep_tracking/Oep_Track.asmx"
+    OCA_WDSL = "%s?WSDL" % WSDL_BASE_URI
     LABELS_FOR_INTEGERS = [
-        'Numero',
-        'Piso',
-        'idCentroImposicion',
-        'idTipoSercicio',
-        'PlazoEntrega',
-        'Tarifador'
+        "Numero",
+        "Piso",
+        "idCentroImposicion",
+        "idTipoSercicio",
+        "PlazoEntrega",
+        "Tarifador",
     ]
-    LABELS_FOR_FLOATS = [
-        'Precio',
-        'Adicional',
-        'Total'
-    ]
-    LABELS_FOR_DATETIMES = [
-        'fecha'
-    ]
+    LABELS_FOR_FLOATS = ["Precio", "Adicional", "Total"]
+    LABELS_FOR_DATETIMES = ["fecha"]
 
     OPERATIVAS = {
-        72767: 'Punto a Punto Estandar con Seguro / Pago en Destino',
-        72768: 'Punto a Sucursal Estandar con Seguro / Pago en Destino',
-        72769: 'Punto a Punto Prioritario con Seguro / Pago en Destino',
-        72770: 'Punto a Sucursal Prioritario con Seguro / Pago en Destino',
-        72771: 'Punto a Punto Estandar / Pago en Destino',
-        72772: 'Punto a Sucursal Estandar / Pago en Destino',
-        72951: 'Punto a Punto Estandar con Seguro / Pago en Destino',
-        72952: 'Punto a Sucursal Estandar con Seguro / Pago en Destino',
-        72952: 'Punto a Punto Estandar con Seguro / Pago en Destino',
-        72953: 'Punto a Punto Prioritario con Seguro / Pago en Destino',
-        72952: 'Punto a Sucursal Prioritario con Seguro / Pago en Destino'
+        72767: "Punto a Punto Estandar con Seguro / Pago en Destino",
+        72768: "Punto a Sucursal Estandar con Seguro / Pago en Destino",
+        72769: "Punto a Punto Prioritario con Seguro / Pago en Destino",
+        72770: "Punto a Sucursal Prioritario con Seguro / Pago en Destino",
+        72771: "Punto a Punto Estandar / Pago en Destino",
+        72772: "Punto a Sucursal Estandar / Pago en Destino",
+        72951: "Punto a Punto Estandar con Seguro / Pago en Destino",
+        72952: "Punto a Sucursal Estandar con Seguro / Pago en Destino",
+        72952: "Punto a Punto Estandar con Seguro / Pago en Destino",
+        72953: "Punto a Punto Prioritario con Seguro / Pago en Destino",
+        72952: "Punto a Sucursal Prioritario con Seguro / Pago en Destino",
     }
 
     FRANJAS_HORARIAS = {
-        1: '8 a 17hs',
-        2: '8 a 12hs',
-        3: '14 a 17hs',
+        1: "8 a 17hs",
+        2: "8 a 12hs",
+        3: "14 a 17hs",
     }
 
     @classmethod
     def iterateresult(cls, id_result, xml_result):
-        tree = ET.fromstring(
-            str(
-                xml_result[id_result].as_xml()
-            )
-        )
+        tree = ET.fromstring(str(xml_result[id_result].as_xml()))
         r = []
-        for node in tree.getiterator('Table'):
+        for node in tree.getiterator("Table"):
             t = {}
             for field in node.getiterator():
-                if field.tag != 'Table' and field.text:
+                if field.tag != "Table" and field.text:
                     value = field.text.strip()
                     if value:
                         if field.tag in OcaService.LABELS_FOR_INTEGERS:
@@ -69,12 +61,9 @@ class OcaService(object):
                         elif field.tag in OcaService.LABELS_FOR_FLOATS:
                             value = float(value)
                         elif field.tag in OcaService.LABELS_FOR_DATETIMES:
-                            value = datetime.strptime(
-                                value,
-                                '%Y-%m-%dT%H:%M:%S-03:00'
-                            )
+                            value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S-03:00")
                         else:
-                            value = value.encode('utf-8')
+                            value = value.encode("utf-8")
                         t[field.tag] = value
             r.append(t)
         return r
@@ -92,79 +81,73 @@ class OcaService(object):
 
     def anularOrdenGenerada(self, orden):
         return OcaService.iterateresult(
-            'AnularOrdenGeneradaResult',
+            "AnularOrdenGeneradaResult",
             self.client.AnularOrdenGenerada(
-                usuario=self.user,
-                password=self.password,
-                nroOrden=orden
-            )
+                usuario=self.user, password=self.password, nroOrden=orden
+            ),
         )
 
     def centroCostoPorOperativa(self, operativa):
         return OcaService.iterateresult(
-            'GetCentroCostorPorOperativaResult',
+            "GetCentroCostorPorOperativaResult",
             self.client.GetCentroCostorPorOperativa(
-                CUIT=self.CUIT,
-                Operativa=operativa
-            )
+                CUIT=self.CUIT, Operativa=operativa
+            ),
         )
 
     def centrosDeImposicion(self):
         return OcaService.iterateresult(
-            'GetCentrosImposicionResult',
-            self.client.GetCentrosImposicion()
+            "GetCentrosImposicionResult", self.client.GetCentrosImposicion()
         )
 
     def centrosDeImposicionPorCP(self, cp):
         return OcaService.iterateresult(
-            'GetCentrosImposicionPorCPResult',
-            self.client.GetCentrosImposicionPorCP(
-                CodigoPostal=cp
-            )
+            "GetCentrosImposicionPorCPResult",
+            self.client.GetCentrosImposicionPorCP(CodigoPostal=cp),
         )
 
-    def ingresarOR(self, compra, dias_retiro, franja_horaria,
-                   confirmar_retiro=False):
-        t_file = open(os.path.join(ROOT, 'templates', 'retiro.xml'))
+    def ingresarOR(self, compra, dias_retiro, franja_horaria, confirmar_retiro=False):
+        t_file = open(os.path.join(ROOT, "templates", "retiro.xml"))
         template = [l for l in t_file.readlines()]
-        template = Template(''.join(template))
-        template_render = template.render(compra).replace('\t', '').replace('\n', '')
-        print template_render
+        template = Template("".join(template))
+        template_render = template.render(compra).replace("\t", "").replace("\n", "")
+        logger.info(template_render)
         r = self.client.IngresoOR(
             usr=self.user,
             psw=self.password,
             XML_Retiro=template_render,
-            ConfirmarRetiro='true' if confirmar_retiro else 'false',
+            ConfirmarRetiro="true" if confirmar_retiro else "false",
             DiasRetiro=dias_retiro,
-            FranjaHoraria=franja_horaria
+            FranjaHoraria=franja_horaria,
         )
         return r
 
     def estadoUltimosEnvios(self, operativas, from_date, to_date):
         return OcaService.iterateresult(
-            'GetEnviosUltimoEstadoResult',
+            "GetEnviosUltimoEstadoResult",
             self.client.GetEnviosUltimoEstado(
                 cuit=self.CUIT,
                 operativas=operativas,
-                fechaDesta=from_date.strftime('%d/%m/%Y'),
-                fechaHasta=to_date.strftime('%d/%m/%Y')
-            )
+                fechaDesta=from_date.strftime("%d/%m/%Y"),
+                fechaHasta=to_date.strftime("%d/%m/%Y"),
+            ),
         )
 
     def listEnvios(self, from_date, to_date):
         return OcaService.iterateresult(
-            'List_EnviosResult',
+            "List_EnviosResult",
             self.client.List_Envios(
                 CUIT=self.CUIT,
-                FechaDesde=from_date.strftime('%d/%m/%Y'),
-                FechaHasta=to_date.strftime('%d/%m/%Y')
-            )
+                FechaDesde=from_date.strftime("%d/%m/%Y"),
+                FechaHasta=to_date.strftime("%d/%m/%Y"),
+            ),
         )
 
-    def tarifarEnvioCorporativo(self, peso_total, volumen_total, cp_origen,
-                                cp_destino, n_paquetes, operativa):
+    def tarifarEnvioCorporativo(
+        self, peso_total, volumen_total, cp_origen, cp_destino, n_paquetes, operativa
+    ):
         return OcaService.iterateresult(
-            'Tarifar_Envio_CorporativoResult',
+            "Tarifar_Envio_CorporativoResult",
             self.client.Tarifar_Envio_Corporativo(
                 PesoTotal=peso_total,
                 VolumenTotal=volumen_total,
@@ -172,32 +155,23 @@ class OcaService(object):
                 CodigoPostalDestino=cp_destino,
                 CantidadPaquetes=n_paquetes,
                 Cuit=self.CUIT,
-                Operativa=operativa
-            )
+                Operativa=operativa,
+            ),
         )
 
     def trackingOR(self, orden_retiro):
         return OcaService.iterateresult(
-            'Tracking_OrdenRetiroResult',
-            self.client.Tracking_OrdenRetiro(
-                CUIT=self.CUIT,
-                OrdenRetiro=orden_retiro
-            )
+            "Tracking_OrdenRetiroResult",
+            self.client.Tracking_OrdenRetiro(CUIT=self.CUIT, OrdenRetiro=orden_retiro),
         )
 
     def trackingPiezaConID(self, pieza):
         return OcaService.iterateresult(
-            'Tracking_PiezaResult',
-            self.client.Tracking_Pieza(
-                Pieza=pieza
-            )
+            "Tracking_PiezaResult", self.client.Tracking_Pieza(Pieza=pieza)
         )
 
     def trackingPiezaConDNICUIT(self, nro_documento, cuit):
         return OcaService.iterateresult(
-            'Tracking_PiezaResult',
-            self.client.Tracking_Pieza(
-                NroDocumentoCliente=nro_documento,
-                CUIT=cuit
-            )
+            "Tracking_PiezaResult",
+            self.client.Tracking_Pieza(NroDocumentoCliente=nro_documento, CUIT=cuit),
         )
